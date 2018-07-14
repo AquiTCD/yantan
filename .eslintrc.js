91747cb29f7f64337c4b25e3b1dbf18c9c8495f5
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-07-14 12:13:12 +0900

    chore(config): update rules of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,50 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   env: {
     browser: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
+    'prettier',
+    'prettier/standard',
+    'plugin:vue/recommended',
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  'rules': {
-    'comma-dangle': [2, 'always-multiline'],
-    'no-console': 1,
-    'eol-last': 0,
-    'block-scoped-var': 0,
+  plugins: ['prettier', 'vue', 'json'],
+  rules: {
+    'prettier/prettier': [
+      1,
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+    'block-scoped-var': 'off',
     'complexity': 1,
-    'consistent-return': 1,
     'default-case': 1,
-    'eqeqeq': 1,
+    'eol-last': 'off',
+    // https://github.com/prettier/eslint-config-prettier#max-len
+    // 'max-len': ['error', { code: 80, ignoreTrailingComments: true, ignoreUrls: true }],
     'no-alert': 1,
-    'no-caller': 1,
-    'no-eval': 2,
-    'no-extra-semi': 1,
-    'no-global-assign': 1,
-    'no-new': 0,
-    'no-new-func': 1,
-    'no-proto': 2,
-    'no-script-url': 1,
-    'no-self-compare': 1,
-    'no-undef': 2,
-    'no-void': 2,
-    'no-var': 2,
-    'camelcase': 1,
-    'key-spacing': [2, {'beforeColon': false, 'afterColon': true, 'align': 'colon'}],
-    'no-array-constructor': 1,
-    'quotes': [1, 'single'],
-    'no-unused-vars': 2,
-    'no-useless-escape': 0,
-    'space-after-keywords': 0,
-    'space-before-blocks': [0, { 'functions': 'always' }],
-    'space-infix-ops': 0,
-    'space-return-throw-case': 0,
-    'comma-spacing': 0,
-    'prefer-const': 0,
-    'curly': 0,
-    'object-curly-spacing': [2, 'always', { 'arraysInObjects': false, 'objectsInObjects': false }],
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
+    'no-console': 1,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 'off',
+    'no-eval': 2,
+    'no-script-url': 1,
+    'no-var': 2,
+    'no-void': 2,
+    'prefer-const': 1,
+    'space-after-keywords': 'off',
+    'space-return-throw-case': 'off'
+  },
 }
